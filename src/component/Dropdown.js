1b1b4413cda8cea6ad9bb9ca1802eeddd5366fe1
Haryo Wisaksono <haryow25@gmail.com>
2022-10-02 18:52:52 +0700

    third commit

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -3,6 +3,8 @@ import "./Dropdown.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMap } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import { AiOutlineCheck } from "react-icons/ai";
+
 function DropdownBar() {
   const [show, setShow] = useState(false);
 
@@ -14,11 +16,17 @@ function DropdownBar() {
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        {}
-        <Dropdown.Item href="">Simple</Dropdown.Item>
-        <Dropdown.Item href="">Satelite</Dropdown.Item>
-        <Dropdown.Item href="">Dark</Dropdown.Item>
+        <Dropdown.Item href="" onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+          Simple
+        </Dropdown.Item>
+        <Dropdown.Item href="" onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+          Satelite
+        </Dropdown.Item>
+        <Dropdown.Item href="" onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+          Dark
+        </Dropdown.Item>
       </Dropdown.Menu>
+      {show && <AiOutlineCheck className="check-button" />}
     </Dropdown>
   );
 }
